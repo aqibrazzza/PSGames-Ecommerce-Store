cd0a62b13fc521338d66da2ba5d99123f7bcee9a
Aqib Raza <aqibraza@outlook.com>
2022-08-14 19:55:27 +0500

    removed flowbite from tailwind config, created dummy responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-	return (
-		<div>
-			<h1 className="text-blue-600 font-bold text-2xl"> PSGames </h1>
-		</div>
-	);
+	return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import { MenuAlt3Icon } from "@heroicons/react/outline";
+import { XIcon } from "@heroicons/react/outline";
+import { useState, useRef, useEffect } from "react";
+
+export default function Navbar() {
+	const [menuIsOpened, setMenuIsOpened] = useState(false);
+	const dropdownMenu = useRef();
+	useEffect(() => {
+		const checkIfClickedOutside = (e) => {
+			// If the menu is opened and the clicked target is not within the menu,
+			// then close the menu
+			if (menuIsOpened && !dropdownMenu.current.contains(e.target)) {
+				setMenuIsOpened(false);
+			}
+		};
+
+		document.addEventListener("mousedown", checkIfClickedOutside);
+
+		return () => {
+			// Cleanup the event listener
+			document.removeEventListener("mousedown", checkIfClickedOutside);
+		};
+	}, [menuIsOpened]);
+
+	return (
+		<nav className="bg-gray-100">
+			<div className="flex justify-between py-3 container">
+				<div className="text-xl font-bold uppercase">Logo</div>
+				<div className="hidden sm:block">
+					<ul className="flex space-x-8">
+						<li className="cursor-pointer text-gray-500 hover:text-gray-700 active:text-gray-600">
+							Home
+						</li>
+						<li className="cursor-pointer text-gray-500 hover:text-gray-700 active:text-gray-600">
+							About
+						</li>
+						<li className="cursor-pointer text-gray-500 hover:text-gray-700 active:text-gray-600">
+							Careers
+						</li>
+						<li className="cursor-pointer text-gray-500 hover:text-gray-700 active:text-gray-600">
+							Contact
+						</li>
+					</ul>
+				</div>
+				<div ref={dropdownMenu} className="sm:hidden relative">
+					{!menuIsOpened && (
+						<MenuAlt3Icon
+							className="h-7 w-7"
+							strokeWidth="1.5"
+							onClick={() => {
+								setMenuIsOpened((prevState) => !prevState);
+							}}
+						/>
+					)}
+					{menuIsOpened && (
+						<XIcon
+							className="h-7 w-7"
+							strokeWidth="1.5"
+							onClick={() => {
+								setMenuIsOpened((prevState) => !prevState);
+							}}
+						/>
+					)}
+					<div
+						className={`absolute top-14 right-1 bg-gray-100 py-4 px-2 rounded ${
+							!menuIsOpened && "hidden"
+						}`}
+					>
+						<ul className="space-y-2">
+							<li className="cursor-pointer py-1 px-8 active:bg-gray-200 text-gray-500 hover:text-gray-700 active:text-gray-600">
+								Home
+							</li>
+							<li className="cursor-pointer py-1 px-8 active:bg-gray-200 text-gray-500 hover:text-gray-700 active:text-gray-600">
+								About
+							</li>
+							<li className="cursor-pointer py-1 px-8 active:bg-gray-200 text-gray-500 hover:text-gray-700 active:text-gray-600">
+								Careers
+							</li>
+							<li className="cursor-pointer py-1 px-8 active:bg-gray-200 text-gray-500 hover:text-gray-700 active:text-gray-600">
+								Contact
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
