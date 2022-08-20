a1e6ca279dbe924b17d52154b67cc3819ccd8f9d
Aqib Raza <aqibraza@outlook.com>
2022-08-20 23:41:05 +0500

    Added json-server, added axios, removed static dummy products & fixed game details page.

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,9 +1,8 @@
 import GameItem from "./GameItem";
-import { games } from "../dummyGames";
-export default function GameList() {
+export default function GameList({ games }) {
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
-			{games.map((game) => {
+			{games?.map((game) => {
 				return <GameItem key={game.id} game={game} />;
 			})}
 		</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import GameList from "../components/GameList";
 
 export default function Home() {
+	const [games, setGames] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState(null);
+
+	const fetchGames = async () => {
+		try {
+			setError(null);
+			setIsPending(true);
+			const res = await axios.get("http://localhost:3000/games");
+			setGames(res.data);
+			setIsPending(false);
+		} catch (err) {
+			setIsPending(false);
+			setError(err.message);
+		}
+	};
+
+	useEffect(() => {
+		fetchGames();
+	}, []);
+
 	return (
 		<div className="container pt-24 space-y-8">
 			<h1 className="text-lg font-medium text-slate-600">Latest Games</h1>
-			<GameList />
+			{isPending && (
+				<img
+					className="mx-auto my-auto"
+					src="images/spinner-circle.svg"
+					alt="loading spinner"
+				/>
+			)}
+			{error && <div> {error} </div>}
+			<GameList games={games} />
 		</div>
 	);
 }
