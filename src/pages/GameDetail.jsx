28482ef5b1e617feec5a3794919e5c2715681bed
Aqib Raza <aqibraza@outlook.com>
2022-09-01 23:47:54 +0500

    Added product quantity selector

--- src/pages/GameDetail.jsx
+++ src/pages/GameDetail.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getGameDetails } from "../features/games/gamesSlice";
 
 export default function GameDetail() {
+	const [qty, setQty] = useState(0);
 	// Get the game id param from the URL.
 	let { id } = useParams();
 
@@ -19,7 +20,7 @@ export default function GameDetail() {
 
 	useEffect(() => {
 		dispatch(getGameDetails(id));
-	}, []);
+	}, [dispatch]);
 
 	return (
 		<div className="container py-28 md:flex md:justify-around md:space-x-20">
@@ -37,7 +38,7 @@ export default function GameDetail() {
 					</div>
 					<div className="mt-12 md:mt-0">
 						<div className="space-y-2">
-							<h1 className="text-3xl tracking-tight font-semibold">
+							<h1 className="text-3xl tracking-tight font-semibold max-w-md">
 								{game.name}
 							</h1>
 							<p className="flex">
@@ -46,17 +47,57 @@ export default function GameDetail() {
 							</p>
 						</div>
 						<p className="mt-4 max-w-md">{game.description}</p>
-						<div className="py-8 space-y-1">
-							<p className="text-lg tracking-tight font-semibold space-x-2">
+						<div className="py-8">
+							<p className="text-lg font-semibold space-x-2">
 								<span>Price:</span>
 								<span>${game.price}</span>
 							</p>
-							<p className="text-sm tracking-tight font-semibold space-x-2 text-slate-500">
-								<span>In stock:</span>
-								<span>{game.inStock} in stock</span>
+							<p className="text-sm font-semibold space-x-2 text-slate-500">
+								{game.inStock > 0 ? (
+									`In stock: ${game.inStock}`
+								) : (
+									<span>Out of stock</span>
+								)}
 							</p>
+							{game.inStock > 0 && (
+								<form className="text-md font-semibold space-x-2 mt-6 py-1 px-2 text-slate-700 border w-fit rounded-lg">
+									<label htmlFor="qty"> Qty:</label>
+									<select
+										name="qty"
+										id="qty"
+										value={qty}
+										onChange={(e) => setQty(e.target.value)}
+									>
+										{[...Array(game.inStock).keys()].map(
+											(key) => {
+												return (
+													<option
+														key={key + 1}
+														value={key + 1}
+													>
+														{key + 1}
+													</option>
+												);
+											}
+										)}
+										1
+									</select>
+								</form>
+							)}
 						</div>
-						<button className="btn"> Add to Cart</button>
+
+						{game?.inStock > 0 ? (
+							<button className="btn cursor-pointer">
+								Add to Cart
+							</button>
+						) : (
+							<button
+								className="btn disabled:pointer-events-none opacity-50"
+								disabled
+							>
+								Add to Cart
+							</button>
+						)}
 					</div>
 				</>
 			)}
